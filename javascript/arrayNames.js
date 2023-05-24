34eaf83a385bf2090531daf1561716904f70d581
Jeaneth Sanchez Nuñez <jeaneth.sanchez@gmail.com>
2023-05-24 17:42:33 +0200

    posicionamiento cabeza

--- javascript/arrayNames.js
+++ javascript/arrayNames.js
@@ -5,8 +5,8 @@
 //var names = ["Nombre 1", "Nombre 2", "Nombre 3"];
 
 //Array con nombres:
-const arrayNames = ["sylvia", "gigi", "rafa", "neema", "genesis", "florencia", "lea", "cindy", "monica", "rosa p.", "laura", "rosa r.",
-    "rosemary", "laura", "manuela", "lorena", "denise", "valentina", "lucía", "teresa", "jacinemi", "zoraida", "alex", "laura"
+const arrayNames = ["Sylvia", "Gigi", "Rafa", "Neema", "Genesis", "Florencia", "Lea", "Cindy", "Monica", "Rosa p.", "Laura", "Rosa r.",
+    "Rosemary", "Laura", "Manuela", "Lorena", "Denise", "Valentina", "Lucía", "Teresa", "Jacinemi", "Zoraida", "Alex", "Laura"
 ];
 
 /*
